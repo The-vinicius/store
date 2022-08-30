c319e625bf0e20bf15521fb96ef6b4bd9ae96071
The-vinicius <viniciusnao41@gmail.com>
2022-08-30 14:50:40 +0000

    create view categorylist for show categories of products

--- core/products/views.py
+++ core/products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.edit import UpdateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Category, Product
 
-# Create your views here.
+
+class CategoryView(ListView):
+    queryset = Category.objects.all()
+    paginate_by = 12
+    template_name = 'products/category_list.html'
+
