b15e96df3a495ab41bce998083d62fd0b0de87fa
The-vinicius <viniciusnao41@gmail.com>
2022-08-31 19:02:04 +0000

    add url list_product

--- core/products/urls.py
+++ core/products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductFormView
+from .views import ProductFormView, ListProductView
 
 app_name = 'product'
 
 urlpatterns = [
     path('add-product', ProductFormView.as_view(), name='add'),
+    path('todos/', ListProductView.as_view(), name='list_product'),
 ]
