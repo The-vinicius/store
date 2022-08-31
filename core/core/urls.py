9ec91bf9c938298bfdb5c710dfeedc1545677d69
The-vinicius <viniciusnao41@gmail.com>
2022-08-31 09:00:34 +0000

    add urls app products urls

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from products.views import CategoryView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('categorias/', CategoryView.as_view(), name='category'),
+    path('produtos/', include('products.urls'))
 ]
 
 if settings.DEBUG:
