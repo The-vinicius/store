e4eb51bc7e48e8046a8222dffaf76837550c67e8
The-vinicius <viniciusnao41@gmail.com>
2022-08-31 08:54:38 +0000

    import createview, create view productformview

--- core/products/views.py
+++ core/products/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, CreateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Category, Product
+from .forms import ProductForm
 
 
 class CategoryView(ListView):
     queryset = Category.objects.all()
     paginate_by = 12
     template_name = 'products/category_list.html'
+
+
+class ProductFormView(CreateView):
+    template_name = 'products/product_form.html'
+    form_class = ProductForm
+    success_url = '/categorias/'
 
