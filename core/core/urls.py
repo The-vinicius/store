3adf12f8d0d9451380872430e883274e9949890e
The-vinicius <viniciusnao41@gmail.com>
2022-08-31 08:58:25 +0000

    add url cotegorias

--- core/core/urls.py
+++ core/core/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from products.views import CategoryView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('categorias/', CategoryView.as_view(), name='category'),
 ]
 
 if settings.DEBUG:
