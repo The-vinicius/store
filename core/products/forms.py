0260ec9a70fb8717d695653b324adb4b2d0a19bd
The-vinicius <viniciusnao41@gmail.com>
2022-09-01 15:02:45 +0000

    form image accept multiple images

--- core/products/forms.py
+++ core/products/forms.py
@@ -3,6 +3,7 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    image = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple':True}))
     class Meta:
         model = Product
         fields = ('name', 'description', 'category', 'price', 'is_available', 'image')
