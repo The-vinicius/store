0e5dd7d55b0572ac976be88f36715e93d0e124ae
The-vinicius <viniciusnao41@gmail.com>
2022-09-09 16:00:10 +0000

    test detail view status code 200

--- core/products/tests/test_views.py
+++ core/products/tests/test_views.py
@@ -1,6 +1,6 @@
 from pytest import mark
 from django.urls import reverse
-from ..models import Category
+from ..models import Category, Product
 
 @mark.django_db
 def test_category_views_status_code_200(client):
@@ -15,5 +15,22 @@ def test_category_list_views_status_code_200(client):
     category = Category.objects.create(name='clock')
     # gera url para category_list com objeto category
     url = reverse('category_list', kwargs={'slug': category.slug})
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@mark.django_db
+def test_product_detail_view_status_code_200(client):
+    category = Category.objects.create(name='clock')
+    # create product
+    product = Product.objects.create(
+        name='besta',
+        description='qualquer coisa?',
+        price=12345,
+        category=category,
+        image='/home/zeus/Imagens/tesla_car_PNG46.png',
+       )
+    # gera url para detail com objeto product
+    url = reverse('products:detail', kwargs={'slug': product.slug})
     response = client.get(url)
     assert response.status_code == 200
