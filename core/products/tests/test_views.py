96af6b7bb5f00ca51ade733aff68145b86b31ad4
The-vinicius <viniciusnao41@gmail.com>
2022-09-09 15:17:32 +0000

    test category_list with category clock

--- core/products/tests/test_views.py
+++ core/products/tests/test_views.py
@@ -7,3 +7,13 @@ def test_category_views_status_code_200(client):
     url = reverse('category')
     response = client.get(url)
     assert response.status_code == 200
+
+
+@mark.django_db
+def test_category_list_views_status_code_200(client):
+    # create category
+    category = Category.objects.create(name='clock')
+    # gera url para category_list com objeto category
+    url = reverse('category_list', kwargs={'slug': category.slug})
+    response = client.get(url)
+    assert response.status_code == 200
