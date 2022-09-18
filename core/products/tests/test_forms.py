8ad001ce81af496b48e7f2ecad2856c112d07541
The-vinicius <viniciusnao41@gmail.com>
2022-09-18 20:46:49 +0000

    test post product

--- core/products/tests/test_forms.py
+++ core/products/tests/test_forms.py
@@ -2,16 +2,28 @@ import pytest
 from django.urls import reverse
 from ..forms import ProductForm
 from ..models import Product
+from rolepermissions.roles import assign_role
+from rolepermissions.permissions import grant_permission
+
 
 @pytest.mark.django_db
-def test_product_form_is_valid(client, category, product):
-    data = {'name': product.name,
-            'description': product.description,
-            'category': product.category,
-            'price': product.price,
-            'is_available': product.is_available,
-            'image': '/home/zeus/kali-linux-wallpaper-hd-69-images.jpg',
-           }
-    url = reverse('products:add')
+def test_post_product_with_permissions_gerente_response_status_code_200(
+    category, product, django_user_model, client
+):
+    user = django_user_model.objects.create(username="someone", password="pass")
+    # user permissions of gerente
+    assign_role(user, "gerente")
+    # force login
+    client.force_login(user=user)
+
+    data = {
+        "name": product.name,
+        "description": product.description,
+        "category": product.category,
+        "price": product.price,
+        "is_available": product.is_available,
+        "image": "/home/zeus/kali-linux-wallpaper-hd-69-images.jpg",
+    }
+    url = reverse("products:add")
     response = client.post(url, data)
     assert response.status_code == 200
