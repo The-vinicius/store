aa3c008e838f7ed9ebdb270f3fd7bdae5e0540d5
The-vinicius <viniciusnao41@gmail.com>
2022-10-07 11:59:06 +0000

    created url filter

---
+++ core/filter/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import FilterProductView
+
+app_name = "filter"
+
+urlpatterns = [
+    path("<slug:slug>_mais<int:price_gt>_menos<int:price_lt>/", FilterProductView.as_view(), name="result")
+]
