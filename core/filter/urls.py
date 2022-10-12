b8a61dbf614bc7f844ee7f87a1d8643295a34d6f
The-vinicius <viniciusnao41@gmail.com>
2022-10-12 18:19:35 +0000

    black

--- core/filter/urls.py
+++ core/filter/urls.py
@@ -4,5 +4,9 @@ from .views import FilterProductView
 app_name = "filter"
 
 urlpatterns = [
-    path("<slug:slug>_mais<int:price_gt>_menos<int:price_lt>/", FilterProductView.as_view(), name="result")
+    path(
+        "<slug:slug>_mais<int:price_gt>_menos<int:price_lt>/",
+        FilterProductView.as_view(),
+        name="result",
+    ),
 ]
