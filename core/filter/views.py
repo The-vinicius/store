699b1bc8d024e69532b9bbf1a2960d1f41ec27c1
The-vinicius <viniciusnao41@gmail.com>
2022-10-12 18:15:36 +0000

    view flter

--- core/filter/views.py
+++ core/filter/views.py
@@ -1,6 +1,8 @@
 from django.views.generic.list import ListView
 from products.models import Product, Category
 from django.shortcuts import get_object_or_404
+from products.utils import FilterPrice
+from django.db.models import Max
 
 
 class FilterProductView(ListView):
@@ -8,21 +10,35 @@ class FilterProductView(ListView):
     paginate_by = 20
 
     def get_queryset(self):
+        # var global get_context_data
+        global price
+        global category_slug
+        
         queryset = Product.available.all()
         category_slug = self.kwargs.get("slug")
-        print(self.kwargs)
-        # pegando os valores do form        
-        gt = self.kwargs.get('price_gt')#great than
-        lt = self.kwargs.get('price_lt')#less than
+
+        # pegando os valores do form
+        gt = self.kwargs.get("price_gt")  # great than
+        lt = self.kwargs.get("price_lt")  # less than
 
         if not gt:
             gt = 0
-
+        # gt can't lt
         if not lt or int(gt) > int(lt):
             lt = 1000000
 
         if category_slug:
             self.category = get_object_or_404(Category, slug=category_slug)
-            queryset = queryset.filter(category=self.category, price__gt=gt, price__lt=lt)
+            queryset = queryset.filter(
+                category=self.category, price__gt=gt, price__lte=lt
+            )
+        # get filter price
+        price = FilterPrice(queryset)
 
         return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["slug"] = category_slug
+        context["price"] = price
+        return context
