3c72123e01c4d1091e691768165c9efb2b8769e3
The-vinicius <viniciusnao41@gmail.com>
2022-10-20 18:39:53 +0000

    open end close modal js

---
+++ core/static/products/js/modal.js
@@ -0,0 +1,23 @@
+// Get body tag
+let boding = document.getElementById('no');
+
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close-own")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+  boding.style.overflow = "hidden";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  boding.style.overflow = "auto";
+}
