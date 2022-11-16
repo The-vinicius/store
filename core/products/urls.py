e390525e5d9234199fb4c3a47655c456c73dfa2f
The-vinicius <viniciusnao41@gmail.com>
2022-11-16 10:23:57 +0000

    added edit url

--- core/products/urls.py
+++ core/products/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
-from .views import ProductFormView, ListProductView, ProductDetailView
+from .views import ProductFormView, ListProductView, ProductDetailView, edit_product
 from rolepermissions.decorators import has_permission_decorator
 
 
-app_name = 'products'
+app_name = "products"
 
 urlpatterns = [
-    path('add-product',
-         has_permission_decorator('add_product')(ProductFormView.as_view()), name='add'),
-    path('todos/', ListProductView.as_view(), name='list_product'),
-    path('<slug:slug>/', ProductDetailView.as_view(), name='detail'),
+    path(
+        "add-product",
+        has_permission_decorator("add_product")(ProductFormView.as_view()),
+        name="add",
+    ),
+    path("todos/", ListProductView.as_view(), name="list_product"),
+    path("<slug:slug>/", ProductDetailView.as_view(), name="detail"),
+    path("edit/<pk>/", edit_product, name="edit"),
 ]
