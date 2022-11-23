3e60cda461b722db975ddecf138c7ea1bfb4b8a4
The-vinicius <viniciusnao41@gmail.com>
2022-11-23 21:34:29 +0000

    created url delete

--- core/products/urls.py
+++ core/products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProductFormView, ListProductView, ProductDetailView, edit_product
+from .views import ProductFormView, ListProductView, ProductDetailView, edit_product, ProductDeleteView
 from rolepermissions.decorators import has_permission_decorator
 
 
@@ -14,4 +14,5 @@ urlpatterns = [
     path("todos/", ListProductView.as_view(), name="list_product"),
     path("<slug:slug>/", ProductDetailView.as_view(), name="detail"),
     path("edit/<pk>/", has_permission_decorator("edit_product")(edit_product), name="edit"),
+    path("apagar/<int:pk>/", ProductDeleteView.as_view(), name='delete'),
 ]
