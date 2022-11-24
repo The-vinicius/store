fe40415de36c7c5075bd6cf11cc3351b095ef8b4
The-vinicius <viniciusnao41@gmail.com>
2022-11-24 15:11:23 +0000

    urls app category

--- core/core/urls.py
+++ core/core/urls.py
@@ -2,13 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from products.views import CategoryView, CategoryProductView
 from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('categorias/', CategoryView.as_view(), name='category'),
-    path('categorias/<slug:slug>/', CategoryProductView.as_view(), name='category_list'),
+    path('categorias/', include('category.urls')),
     path('produtos/', include('products.urls')),
     path('resultado/', include('filter.urls')),
     path('', home, name='home'),
