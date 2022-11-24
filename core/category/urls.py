68cc2bbcb4088f240d09607adbfdefd803a0d8c3
The-vinicius <viniciusnao41@gmail.com>
2022-11-24 15:10:03 +0000

    url category_list product

--- core/category/urls.py
+++ core/category/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CategoryView
+from .views import CategoryView, CategoryProductView
 
 app_name = "category"
 
 urlpatterns = [
-    path("", CategoryView.as_view(), name="category")
+    path("", CategoryView.as_view(), name="categories"),
+    path('<slug:slug>/', CategoryProductView.as_view(), name='category_list'),
 ]
