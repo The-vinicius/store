3aecd29f3f1f5cebd0b2cf0fb71da7ce9e257da8
The-vinicius <viniciusnao41@gmail.com>
2022-11-25 10:43:19 +0000

    form category

--- core/category/forms.py
+++ core/category/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from products.models import Category
 
 class CategoryForm(forms.ModelForm):
+    image = forms.ImageField(
+        widget=forms.ClearableFileInput(
+            attrs={"class": "d-none", "onchange": "image_select();"}
+        )
+    )
+
     class Meta:
         model = Category
         fields = ('name', 'image')
