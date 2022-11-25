e291e2ff8623a75c09ec73a4ad593151b19471cc
The-vinicius <viniciusnao41@gmail.com>
2022-11-25 10:38:24 +0000

    save form category

--- core/category/views.py
+++ core/category/views.py
@@ -45,3 +45,17 @@ class CategoryFormView(FormView):
     template_name = 'category/category_form.html'
     form_class = CategoryForm
     success_url = reverse_lazy('category:categories')
+
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        self.object = form.save()
+        return super().form_valid(form)
+
