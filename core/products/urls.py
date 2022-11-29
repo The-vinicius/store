c4133380c7ae955a8d16fd85e9ab9da37a1834fa
The-vinicius <viniciusnao41@gmail.com>
2022-11-29 10:26:55 +0000

    name url add_product

--- core/products/urls.py
+++ core/products/urls.py
@@ -13,9 +13,9 @@ app_name = "products"
 
 urlpatterns = [
     path(
-        "add-product",
+        "add-produto",
         has_permission_decorator("add_product")(ProductFormView.as_view()),
-        name="add",
+        name="add_product",
     ),
     path("todos/", ListProductView.as_view(), name="list_product"),
     path("see/<slug:slug>/", ProductDetailView.as_view(), name="detail"),
--- core/tests/test_forms.py
+++ core/tests/test_forms.py
@@ -9,7 +9,7 @@ def test_post_product_with_permissions_gerente_response_url_categorias(
     category, user_gerente, client
 ):
     client.force_login(user=user_gerente)
-    url = reverse("products:add")
+    url = reverse("products:add_product")
     with open('tests/img/test.jpg', 'rb') as img:
         data = {
             "name": 'vini',
@@ -26,14 +26,14 @@ def test_post_product_with_permissions_gerente_response_url_categorias(
 @pytest.mark.django_db
 def test_url_add_product_no_permissions_gerente_response_status_code_403(client, user):
     client.force_login(user=user)
-    url = reverse("products:add")
+    url = reverse("products:add_product")
     response = client.get(url)
     assert response.status_code == 403
 
 
 @pytest.mark.django_db
 def test_url_add_product_no_login_response_status_code_403(client):
-    url = reverse("products:add")
+    url = reverse("products:add_product")
     response = client.get(url)
     assert response.status_code == 403
 
@@ -44,7 +44,7 @@ def test_url_add_product_with_permissions_gerente_response_status_code_200(
 ):
     client.force_login(user=user_gerente)
 
-    url = reverse("products:add")
+    url = reverse("products:add_product")
     response = client.get(url)
     assert response.status_code == 200
 
