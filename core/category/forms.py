0b0386ac743dea542400e201b2393688e6d452fd
The-vinicius <viniciusnao41@gmail.com>
2022-11-29 12:03:11 +0000

    form style form-control

--- core/category/forms.py
+++ core/category/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from products.models import Category
 
 class CategoryForm(forms.ModelForm):
+    name = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "form-control"}),
+        max_length=100,
+        required=True,
+    )
     image = forms.ImageField(
         widget=forms.ClearableFileInput(
             attrs={"class": "d-none", "onchange": "image_select();"}
