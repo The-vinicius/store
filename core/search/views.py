a51b7def28f5c3fff98ae88ba3294c0d3c3ce152
The-vinicius <viniciusnao41@gmail.com>
2022-11-30 21:28:13 +0000

    created view search

--- core/search/views.py
+++ core/search/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from products.models import Product
+from django.db.models import Q
 
-# Create your views here.
+
+class SearchProductView(ListView):
+    template_name = 'search/search.html'
+
+
+    def get_queryset(self, *args, **kwargs):
+        query = self.request.GET.get('q', None)
+        lookups = (
+            Q(name__icontains=query)|
+            Q(description__icontains=query)
+        )
+        queryset = Product.objects.filter(lookups)
+        return queryset
