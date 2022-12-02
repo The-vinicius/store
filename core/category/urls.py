c543f9d6df9d7edd0192b8ee32311fb024dd12eb
The-vinicius <viniciusnao41@gmail.com>
2022-12-02 11:06:14 +0000

    cache page redis url categories

--- core/category/urls.py
+++ core/category/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import CategoryView, CategoryProductView, CategoryFormView
 from rolepermissions.decorators import has_permission_decorator
+from django.views.decorators.cache import cache_page
 
 app_name = "category"
 
 urlpatterns = [
-    path("", CategoryView.as_view(), name="categories"),
+    path("", cache_page(60*15)(CategoryView.as_view()), name="categories"),
     path('list/<slug:slug>/', CategoryProductView.as_view(), name='category_list'),
     path(
         'criar/',
