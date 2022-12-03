08568791e0d1b482b580d6cf9dd07124cef22bfc
The-vinicius <viniciusnao41@gmail.com>
2022-12-03 09:52:43 +0000

    deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python core/manager.py migrate && web: gunicorn core/core.wsgi
--- core/core/settings.py
+++ core/core/settings.py
@@ -22,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'test123' # config('SECRET_KEY') 
+config('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = []
-
+#CSRF_TRUSTED_ORIGINS = ['https://f8b0-177-11-159-172.sa.ngrok.io']
 
 # Application definition
 
